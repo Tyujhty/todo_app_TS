01c1ea9d58268b5561ea3ea4daf3ba1d7946c37d
Fanny Baquey <fanny.baquey@gmail.com>
2023-02-20 18:46:00 +0100

    feat: Add edit mode and save function

--- src/assets/ts/animation.ts
+++ src/assets/ts/animation.ts
@@ -6,21 +6,27 @@
 
 const ul = document.querySelector('ul')
 
-const todoList: { text: string, done: boolean } [] = [
+const todoList: { text: string, done: boolean, editMode: boolean } [] = [
     {
         text: 'Apprendre le Javascript',
-        done: true
+        done: true,
+        editMode: true
     },
     {
         text: 'Apprendre le typescript',
-        done: false
+        done: false,
+        editMode: false
     }
 ]
 
 //The function takes a list of todos (todoList) and creates a HTML element for each todo.
 function displayTodo() {
-    let todosNode = todoList.map((todo, index) => {
-        return createTodoElementHtml(todo, index)
+    const todosNode = todoList.map((todo, index) => {
+        if(todo.editMode) {
+            return createEditTodoElement(todo, index)
+        } else {
+            return createTodoElementHtml(todo, index)
+        }
     })
     ul.innerHTML = ''
     ul.append(...todosNode)
@@ -28,26 +34,36 @@ function displayTodo() {
 
 function createTodoElementHtml(todo: any, index: number) {
     const li = document.createElement('li')
+    
     const deleteBtn = document.createElement('button')
     deleteBtn.innerHTML = 'Supprimer'
 
     deleteBtn.addEventListener('click', (event) => {
+        event.stopPropagation()
+
         deleteToDoElement(index)
-        displayTodo()
     })
+
+    const editBtn = document.querySelector('button')
+    editBtn.innerHTML = 'Editer'
+
+    editBtn.addEventListener('click', (event) => {
+        event.stopPropagation()
+
+        openEditMode(index)
+    })
+
 
     li.innerHTML = `
         <span class="todo ${ todo.done ? 'done' : ''}"></span>
         <p>${todo.text}</p>
-        <button>Editer</button>
     `;
 
     li.addEventListener('click', (event) => {
         changeTodoStatut(index)
-        displayTodo()
     })
 
-    li.appendChild(deleteBtn)
+    li.append(editBtn, deleteBtn)
 
     return li
 }
@@ -69,7 +85,6 @@ form.addEventListener('submit', (event) => {
     const inputValue: string = input.value
     input.value = ''
     addTodoElement(inputValue)
-    displayTodo()
 })
 
 //This code creates a new list item element with the given text (input) and appends it.
@@ -78,9 +93,11 @@ function addTodoElement(text: any) {
     todoList.push(
         {
             text,
-            done: false
+            done: false,
+            editMode: false
         }
     )
+    displayTodo()
 }
 
 /* ------------------------------------------------------------------------ 
@@ -88,7 +105,10 @@ function addTodoElement(text: any) {
 -------------------------------------------------------------------------*/
 
 function deleteToDoElement(index: number) {
+
+    // This code removes an item from the todoList array at the specified index. The first argument (index) is the index of the item to be removed, and the second argument (1) is the number of items to be removed.
     todoList.splice(index, 1)
+    displayTodo()
 }
 
 /* ------------------------------------------------------------------------ 
@@ -96,7 +116,65 @@ function deleteToDoElement(index: number) {
 -------------------------------------------------------------------------*/
 
 function changeTodoStatut(index: number) {
-    
+
     //The code takes the index of the item in the todo list and sets the "done" property to the opposite of what it currently is.
-    todoList[index].done = !todoList[index].done
+    todoList[index].editMode = !todoList[index].editMode
+    displayTodo()
+}
+
+/* ------------------------------------------------------------------------ 
+4. Edit toDo
+-------------------------------------------------------------------------*/
+
+function createEditTodoElement(todo: any, index: number) {
+    const li = document.createElement('li')
+    
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.value = todo.text
+    
+    const saveBtn = document.createElement('button')
+    saveBtn.innerHTML = 'Sauvegarder'
+
+    const cancelBtn = document.createElement('button')
+    cancelBtn.innerHTML = 'Annuler'
+
+    saveBtn.addEventListener('click', (event) => {
+        event.stopPropagation()
+
+        saveEditTodo(index, input)
+        displayTodo()
+    })
+
+    cancelBtn.addEventListener('click', (event) => {
+        event.stopPropagation()
+
+        changeTodoStatut(index)
+        displayTodo()
+    })
+
+    li.append(input, saveBtn, cancelBtn)
+    
+    return li
+}
+
+/* ------------------------------------------------------------------------ 
+5. Edit mode button 
+-------------------------------------------------------------------------*/
+
+function openEditMode(index: number) {
+    todoList[index].editMode = true
+    displayTodo()
+}
+
+
+/* ------------------------------------------------------------------------ 
+6. Save edit mode
+-------------------------------------------------------------------------*/
+
+function saveEditTodo(index: number, input: any) {
+    const newInputValue = input.value
+    todoList[index].text = newInputValue
+    todoList[index].editMode = false
+    displayTodo()
 }
